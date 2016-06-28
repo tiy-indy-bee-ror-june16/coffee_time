75de3b078a38a23560edb7f05d1dfe047b3f00ea
dwegrzen <dwegrzen@gmail.com>
2016-06-28 15:39:46 -0400

    completed explorer homework

--- coffee.rb
+++ coffee.rb
@@ -1,3 +1,29 @@
 class Coffee
-  
+
+  attr_accessor :type, :sips
+
+  def initialize(type)
+    self.type = type
+    @sips = 3
+  end
+
+  def full?
+    if @sips == 3
+      true
+    else
+      false
+    end
+  end
+
+  def empty?
+    if @sips == 0
+      true
+    else
+      false
+    end
+  end
+
+  def havedrink
+    @sips = @sips - 1
+  end
 end
--- human.rb
+++ human.rb
@@ -1,11 +1,42 @@
 class Human
   attr_accessor :name,
-                :alertness
+                :alertness,
+                :drinkcount,
+                :drink
 
   def initialize(name, alertness=0)
     self.name = name
     self.alertness = alertness
+    @drink = 0
+    @drinkcount = 0
   end
 
+  def has_coffee?
+    if @drinkcount > 1
+      true
+    else
+      false
+    end
+
+  end
+
+  def needs_coffee?
+    if drinkcount
+      true
+    else
+      false
+    end
+  end
+
+  def buy(drink)
+    @drink = drink
+    @drinkcount = @drinkcount + 1
+  end
+
+  def drink!
+    @drink.havedrink
+    @alertness = @alertness + 0.31
+    @drinkcount = @drinkcount - 1
+  end
 
 end
