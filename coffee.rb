49b8014397833d51ddde3905992e6c7ff7c67edc
Rittersm@gmail.com <rittersm@Seans-MacBook-Pro.local>
2016-06-28 20:25:39 -0400

    Adventure and Epic mode complete

--- coffee.rb
+++ coffee.rb
@@ -24,5 +24,56 @@ class Coffee
       true
     end
   end
+end
 
+class Espresso
+  attr_accessor :name, :caffeine, :volume
+
+  def initialize(name)
+    @name = name
+    @caffeine = 0.4
+    @volume = 1
+  end
+
+  def full?
+    if self.volume == 1
+      true
+    elsif self.volume < 1
+      false
+    end
+  end
+
+  def empty?
+    if self.volume == 0
+      true
+    else
+      false
+    end
+  end
+end
+
+class Tea
+  attr_accessor :name, :caffeine, :volume
+
+  def initialize(name)
+    @name = name
+    @caffeine = 0.25
+    @volume = 4
+  end
+
+  def full?
+    if self.volume == 4
+      true
+    elsif self.volume < 4
+      false
+    end
+  end
+
+  def empty?
+    if self.volume == 0
+      true
+    else
+      false
+    end
+  end
 end
